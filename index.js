951230b14b3a6194759d04790886d470b6e5012f
Josh Smith <josh@Joshs-MacBook-Pro.local>
2016-10-04 22:56:56 -0400

    finally make things work

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var strava = require('strava-v3');
 var alexa = require('alexa-app');
 var strecho = new alexa.app('strecho');
 
+var parser = require('body-parser');
 var app = require('express')();
+
+app.use(parser.json());
 
 // strava.athlete.listActivities({per_page:1}, (err, payload) => {
 //     if (err) {
@@ -15,15 +18,16 @@ var app = require('express')();
 //     console.log(payload);
 // });
 
-strecho.intent('LastActivity', {}, (req, res) => {
+strecho.intent('LastActivity', {}, (request, response) => {
     response.say('you ran so many miles. many many miles. good job.');
 });
 
-app.post('/strecho', (req, res) => {
-    console.log('received post to /strecho');
-    console.log(req.body);
-    strecho.request(req.body).then(res.json);
-});
+strecho.express(app, '', false);
+// app.post('/strecho', (req, res) => {
+//     console.log('received post to /strecho');
+//     console.log(req.body);
+//     strecho.request(req.body).then(res.json, res.send);
+// });
 
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
